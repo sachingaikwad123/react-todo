257f8b57ccc3d42231129e6fefe1b316d3e31521
Sachin Gaikwad <sachingaikwad123@gmail.com>
2023-08-01 16:52:11 +0530

    feat: Add submit button on the page
    - Added submit button on the page
    - Unit-test is written for this

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>My Todo Application</div>;
+  return (
+    <>
+      <div>My Todo Application</div>
+      <button>Submit</button>
+    </>
+  );
 }
 
 export default App;
